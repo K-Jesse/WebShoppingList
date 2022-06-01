7a1679c1ab0d9a19c79a3996f7ce889606dd9a02
Jesse <jesse.a.kulmala@gmail.com>
2022-06-01 09:17:11 +0000

    Add final touches

--- shoppinglist/shoppinglist/settings.py
+++ shoppinglist/shoppinglist/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'ADD YOUR KEY HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'jessekulmala.com', '127.0.0.1']
 
